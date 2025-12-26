9087277772274d1a7fa7bb0d565b01d687f95dd8
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-25 21:53:42 -0600

    added a compoent to the userRepo interface for getting the emails

--- src/main/java/com/zekepeke/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/zekepeke/ecommerce/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.zekepeke.ecommerce.repositories;
 
 import com.zekepeke.ecommerce.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("SELECT u.email FROM User u")
+    List<String> findAllEmails();
 }
