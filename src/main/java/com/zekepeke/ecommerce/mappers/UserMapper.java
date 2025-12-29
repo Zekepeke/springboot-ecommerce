a28ebe5d07151e52971f5d643d41591379947a8b
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-28 21:52:29 -0600

    updated the user mapper

--- src/main/java/com/zekepeke/ecommerce/mappers/UserMapper.java
+++ src/main/java/com/zekepeke/ecommerce/mappers/UserMapper.java
@@ -1,4 +1,10 @@
 package com.zekepeke.ecommerce.mappers;
 
+import com.zekepeke.ecommerce.dtos.UserDto;
+import com.zekepeke.ecommerce.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    UserDto toDto(User user);
 }
