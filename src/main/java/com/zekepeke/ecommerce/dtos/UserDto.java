5d213572228252285e06692f5cfe1973fa356ede
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-28 21:37:18 -0600

    added stuff to the user dto

--- src/main/java/com/zekepeke/ecommerce/dtos/UserDto.java
+++ src/main/java/com/zekepeke/ecommerce/dtos/UserDto.java
@@ -1,9 +1,17 @@
 package com.zekepeke.ecommerce.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+// Need getter so spring can read the data from DTO and create json objects
+@Getter
 // Using Data transfer objects
 public class UserDto {
     private Long id;
     private String name;
     private String email;
+
 
 }
