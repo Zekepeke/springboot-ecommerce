c57196ab24be52fd07a88319accaca4a1166cdeb
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-30 22:59:09 -0600

    finished the product dto

--- src/main/java/com/zekepeke/ecommerce/dtos/ProductDto.java
+++ src/main/java/com/zekepeke/ecommerce/dtos/ProductDto.java
@@ -1,4 +1,14 @@
 package com.zekepeke.ecommerce.dtos;
 
+import com.zekepeke.ecommerce.entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Category category;
 }
