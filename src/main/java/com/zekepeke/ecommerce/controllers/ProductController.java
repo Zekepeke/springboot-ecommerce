ba4e5f1e0394ccc09b5cc6dd35b8deb55d9c0092
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-30 23:06:58 -0600

    Added to the product controller

--- src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
+++ src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
@@ -1,4 +1,19 @@
 package com.zekepeke.ecommerce.controllers;
 
+
+import com.zekepeke.ecommerce.mappers.ProductMapper;
+import com.zekepeke.ecommerce.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+
+
 }
