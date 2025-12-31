49df626ef0d8810fc39a7e025d3c1969e665071e
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-30 23:03:59 -0600

    updated the product mapper

--- src/main/java/com/zekepeke/ecommerce/mappers/ProductMapper.java
+++ src/main/java/com/zekepeke/ecommerce/mappers/ProductMapper.java
@@ -3,9 +3,13 @@ package com.zekepeke.ecommerce.mappers;
 import com.zekepeke.ecommerce.dtos.ProductDto;
 import com.zekepeke.ecommerce.entities.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-
+    @Mapping(
+            source = "category.id",
+            target = "categoryId"
+    )
     ProductDto toDto(Product product);
 }
