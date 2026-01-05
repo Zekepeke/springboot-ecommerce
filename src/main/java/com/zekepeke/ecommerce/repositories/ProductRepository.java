1cddff446375bb7da52f9cb6c649c733e2a16bd6
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-04 21:42:55 -0600

    added a query annonation in the product repo

--- src/main/java/com/zekepeke/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/zekepeke/ecommerce/repositories/ProductRepository.java
@@ -2,9 +2,13 @@ package com.zekepeke.ecommerce.repositories;
 
 import com.zekepeke.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
    List<Product> findByCategoryId(Byte categoryId);
+
+   @Query("SELECT p FROM Product p JOIN FETCH p.category")
+   List<Product> findAllWithCategoru();
 }
