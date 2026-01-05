01c59b39924e3b0ba2ab990108f6806e969d5ec7
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-04 20:48:40 -0600

    added a price of typeBigDecimal

--- src/main/java/com/zekepeke/ecommerce/dtos/ProductDto.java
+++ src/main/java/com/zekepeke/ecommerce/dtos/ProductDto.java
@@ -5,11 +5,14 @@ import com.zekepeke.ecommerce.entities.Category;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @Getter
 public class ProductDto {
     private Long id;
     private String name;
+    private BigDecimal price;
     private String description;
-    private Long categoryId;
+    private Byte categoryId;
 }
