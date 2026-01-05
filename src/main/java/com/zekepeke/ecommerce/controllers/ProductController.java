f293ba3d84fd1381f30f3d531449ead22ceb33eb
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-04 21:44:35 -0600

    chnaged the product controller

--- src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
+++ src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
@@ -31,7 +31,7 @@ public class ProductController {
         if (categoryId != null) {
             products = productRepository.findByCategoryId(categoryId);
         } else {
-            products = productRepository.findAll();
+            products = productRepository.findAllWithCategoru();
         }
        return products.stream().map(productMapper::toDto).toList();
     }
