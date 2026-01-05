d6b6dd25830be3d5c0ccd69d6256b1ddb44232ee
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-04 21:46:12 -0600

    chanhed product repo

--- src/main/java/com/zekepeke/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/zekepeke/ecommerce/repositories/ProductRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+   @EntityGraph(attributePaths = "category")
    List<Product> findByCategoryId(Byte categoryId);
 
 //   @Query("SELECT p FROM Product p JOIN FETCH p.category")
