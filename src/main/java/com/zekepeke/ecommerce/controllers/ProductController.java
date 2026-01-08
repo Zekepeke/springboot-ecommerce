9fed1efc0b1ce874a1d8a25d22bd936423aea808
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-08 03:33:42 -0600

    made the endpoint

--- src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
+++ src/main/java/com/zekepeke/ecommerce/controllers/ProductController.java
@@ -76,4 +76,18 @@ public class ProductController {
         productRepository.save(product);
         return ResponseEntity.ok(productMapper.toDto(product));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductDto> deleteProduct(
+            @PathVariable(name = "id") Long id
+    ) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
+
+    }
 }
